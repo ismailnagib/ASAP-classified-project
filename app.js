a4dc13113c4fba038cae5db6349b1d2f794abc8b
ismail <mismailyusuf@gmail.com>
2018-08-30 20:56:24 +0700

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 
 const Auth = require('./routes/auth')
-// const orderRouter = require('./routes/order')
-
-const session = require('express-session')
+const orderRouter = require('./routes/order')
 
 app.set('view engine', 'ejs')
 
@@ -19,6 +18,6 @@ app.use(session({
 
 app.get('/', (req, res) => {res.redirect('/auth')})
 app.use('/auth', Auth)
-// app.use('/order', orderRouter)
+app.use('/order', orderRouter)
 
 app.listen(3000, () => console.log('Running on port 3000'))
