575feaa478b24013bd358e7eca9728c547024681
Dwi Retno Handayani <retnodayo@Dwis-MacBook-Pro.local>
2018-08-31 07:44:43 +0700

    progress -3, logic di pindah ke controller

--- routes/order.js
+++ routes/order.js
@@ -1,133 +1,20 @@
 const express = require('express'),
       router = express.Router(),
-      OrderModel = require('../models').Order,
-      PackageModel = require('../models').Package
+      OrderController = require('../controllers/orderController')
       
 
 router
-    .get('/' , function(req,res) {
-        OrderModel.findAll({
-            order: [['id','ASC']],
-            include: [{model: PackageModel}],
-            where: {
-                UserId: 1, // Nanti di ganti jadi dinamis
-                isCompleted: false
-            }
-        })
-        .then( orders => {
-            // var yo = new Date().toLocaleTimeString()
-            // res.send(orders[0].deliveredTime.toDateString())
-            
-            // console.log(orders);
-            // res.send(orders)
-            res.render('orderlist-progress', {orders: orders})
-        })
-        .catch(err => {
-            console.log(err)
-            res.send(err.message)
-            
-        })
-    })
+    .get('/' , OrderController.orderListPage)
 
-    .get('/completed' , function(req,res) {
-        OrderModel.findAll({
-            order: [['id','ASC']],
-            include: [{model: PackageModel}],
-            where: {
-                UserId: 2, // Nanti di ganti jadi dinamis
-                isCompleted: true
-            }
-        })
-        .then( orders => {
-            res.render('orderlist-completed', {orders: orders})
-        })
-        .catch(err => {
-            console.log(err)
-            res.send(err.message)
-            
-        })
-    })
+    .get('/completed' , OrderController.completedListPage)
 
-    .get('/add' , function(req,res) {
-       res.render('order-add.ejs')
-    })
+    .get('/add' , OrderController.addOrderPage)
 
-    .post('/add', function(req,res) {
-        let data = req.body;
+    .post('/add', OrderController.addingOrder)
 
-        var price = data.size * data.weight * 700
-        if (data.isFragile === 'true') {
-            price += 30000
-        }
+    .get('/arrived/:id', OrderController.makeOrderComplete)
 
-        OrderModel.create({
-            UserId: null,
-            CourierId: null,
-            PackageId: null,
-            price: price,
-            isCompleted: false,
-            createdAt: new Date,
-            updatedAt: new Date,
-            deliveredTime: new Date
-
-        })
-        .then( () => {
-            PackageModel.create({
-                size: data.size,
-                weight: data.weight,
-                isFragile: data.isFragile
-            })
-            .then( () => {
-                res.redirect('/order')
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.send(err.message)
-            
-        })
-    })
-
-    .get('/arrived/:id', function(req,res) {
-        OrderModel.update({
-            isCompleted: true,
-            arrivedTime: new Date
-        },{
-            where: {
-                id: req.params.id
-            }
-        })
-        .then( () => {
-            res.redirect('/order')
-        })
-        .catch(err => {
-            console.log(err);
-            res.send(err.message)
-        })
-    })
-
-    .get('/cancel/:id', function(req,res) {
-        OrderModel.destroy({
-            where: {
-                id : req.params.id
-            }
-        }) 
-        .then( () => {
-            PackageModel.destroy({
-                where: {
-                    id: req.params.id
-                }
-            })
-            .then( () => {
-                res.redirect('/order')
-            })
-        })
-        .catch(err => {
-            console.log(err);
-            res.send(err.message)          
-        })
-
-    })
+    .get('/cancel/:id', OrderController.cancelOrder)
 
 
 
