845774bc8eb8dfaa734cc4cd3e4ee5342f6f07d4
ismail <mismailyusuf@gmail.com>
2018-08-31 11:11:57 +0700

    add logout feature, add redirection from order if not logged in

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ Routes.get('/', (req, res) => {User.checkLoginAuth(req, res)})
 Routes.post('/', (req, res) => {User.login(req, res)})
 Routes.get('/register', (req, res) => {res.render('register', {message: req.query.message})})
 Routes.post('/register', (req, res) => {User.register(req, res)})
+Routes.get('/logout', (req, res) => {User.logout(req, res)})
 
 Routes.get('/:error', (req, res) => (res.render('error'))) // Selalu taruh paling bawah
 
